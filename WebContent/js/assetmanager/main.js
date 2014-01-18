9bcaf1b3feb0841d51ff88aa12e78bfddbe4b7a5
harao2011 <harao2011@yahoo.co.jp>
2014-01-18 21:01:34 +0900

    project export

---
+++ WebContent/js/assetmanager/main.js
@@ -0,0 +1,157 @@
+$(function() {
+
+    //csv control section
+
+    var AssetManager = {};
+
+    AssetManager.file;
+    AssetManager.loadedData = [];//json
+
+    AssetManager.CONST = {
+        LINE_DIVISION_DEFINE: 0,
+        LINE_HEADER_COLUMN: 1
+    }
+
+    $('#inputSelectFile').bind('change', function(event) {
+        AssetManager.file = event.target.files[0];//multi off
+    });
+
+    $('#inputLoadFile').bind('click', function() {
+        if (!AssetManager.file || !AssetManager.file.type.match('text/csv')) {
+            alert('CSVファイルを選択してください。');
+        }
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            AssetManager.loadedData = [];
+            var resultJsonObject = [];
+            var divisionArray = event.target.result.split('#');
+            //division loop
+            for (var i = 0; i < divisionArray.length; i++) {
+                if (!divisionArray[i] || divisionArray[i] == '') continue;
+                var lineArray = divisionArray[i].split('¥n');
+                var divObj = new Object();
+                divObj.divisionName = lineArray[0].split(',')[0];
+                if (lineArray[0].split(',')[1]) {
+                    divObj.divisionClass = lineArray[0].split(',')[1];
+                }
+
+                var colItems = lineArray[1].split(',');
+                if (!colItems || colItems.length == 0) continue;
+
+                divObj.records = [];
+
+                //TODO: column headerとbodyの長さが違った場合の制御
+
+                //line loop
+                for (var j = 2; j < lineArray.length; j++) {
+                    var record = new Object();
+                    var csvArray = lineArray[j].split(',');
+                    //csv loop
+                    for (var k = 0; k < csvArray.length; k++) {
+                        record[colItems[k]] = csvArray[k];
+                    }
+                    divObj.records.push(record);
+                }
+                resultJsonObject.push(divObj);
+            }
+            AssetManager.loadedData.push(resultJsonObject);
+
+            $("#tableGridTotal").jqGrid({
+                data:resultJsonObject[0].records,
+                datatype : "local",
+                colNames : colNames,
+                colModel : colModelSettings,
+                rowNum : 10,
+                rowList : [1, 10, 20],
+                caption : "Sample Display",
+                height : 200,
+                width : 500,
+                pager : 'divPagerTotal',
+                shrinkToFit : true,
+                viewrecords: true
+            });
+        };
+        reader.readAsText(AssetManager.file, "UTF-8");
+
+    });
+
+
+    //grid control section
+    var colModelSettings= [
+        {name:"date",index:"date",width:70,align:"center",classes:"date_class"},
+        {name:"target",index:"target",width:70,align:"center",classes:"target_class"},
+        {name:"result",index:"result",width:200,align:"center",classes:"name_class"}
+    ];
+
+    var date = [
+        {date:'2013/01',target:150000,result:150000},
+        {date:'2013/02',target:300000,result:320000},
+        {date:'2013/03',target:450000,result:460000},
+        {date:'2013/04',target:600000,result:700000},
+        {date:'2013/05',target:750000,result:800000},
+        {date:'2013/06',target:900000,result:950000},
+        {date:'2013/07',target:1050000,result:1230000},
+        {date:'2013/08',target:1200000,result:1400000},
+        {date:'2013/09',target:1350000,result:1590000},
+        {date:'2013/10',target:1500000,result:1700000},
+        {date:'2013/11',target:1650000,result:1800000},
+        {date:'2013/12',target:1800000,result:2000000}
+
+    ];
+
+    var colNames = ["date","target","result"];
+//    $("#tableGridTotal").jqGrid({
+//        datatype : "json",
+////        colNames : colNames,
+////        colModel : colModelSettings,
+//        rowNum : 10,
+//        rowList : [1, 10, 20],
+//        caption : "Sample Display",
+//        height : 200,
+//        width : 500,
+//        pager : 'divPagerTotal'
+////        shrinkToFit : true,
+////        viewrecords: true
+//    });
+
+    $("#tableGridCash").jqGrid({
+        data:date,
+        datatype : "local",
+        colNames : colNames,
+        colModel : colModelSettings,
+        rowNum : 10,
+        rowList : [1, 10, 20],
+        caption : "Sample Display",
+        height : 200,
+        width : 500,
+        pager : 'pager',
+        shrinkToFit : true,
+        viewrecords: true
+    });
+
+    $("#tableGridInvestment").jqGrid({
+        data:date,
+        datatype : "local",
+        colNames : colNames,
+        colModel : colModelSettings,
+        rowNum : 10,
+        rowList : [1, 10, 20],
+        caption : "Sample Display",
+        height : 200,
+        width : 500,
+        pager : 'pager',
+        shrinkToFit : true,
+        viewrecords: true
+    });
+
+    var Initialize = function() {
+        // Check for the various File API support.
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            //All the File APIs are supported.
+        } else {
+            alert('In this Browser, You cannot use this application.');
+        }
+    };
+
+    Initialize();
+});
---
+++ WebContent/js/assetmanager/main_old.js
@@ -0,0 +1,287 @@
+$(function() {
+	
+	var AssetManager = {};
+    //列の表示名
+    var colNames = ["","No.","name","age","local","hobby"];
+
+    //列の設定
+    var colModelSettings= [	{name:"radio",index:"radio",width:50,align:"center",classes:"radio_class"},
+    {name:"no",index:"no",width:70,align:"center",classes:"no_class"},
+    {name:"name",index:"name",width:200,align:"center",classes:"name_class"},
+    {name:"age",index:"age",width:200,align:"center",classes:"age_class"},
+    {name:"local",index:"local",width:200,align:"center",classes:"local_class"},
+    {name:"hobby",index:"hobby",width:200,align:"center",classes:"hobby_class"}
+    ];
+    
+
+    var date = [
+        {no:1, name:"nakagawa", age:20, local:"Japan", hobby:"Tennis"},
+        {no:2, name:"nakayama", age:20, local:"Japan", hobby:"Soccer"}];
+
+//テーブルの作成
+    $("#mainGrid").jqGrid({
+        data:date,  //表示したいデータ
+        datatype : "local",            //データの種別 他にjsonやxmlも選べます。
+        //しかし、私はlocalが推奨です。
+        colNames : colNames,           //列の表示名
+        colModel : colModelSettings,   //列ごとの設定
+        rowNum : 10,                   //一ページに表示する行数
+        rowList : [1, 10, 20],         //変更可能な1ページ当たりの行数
+        caption : "Sample Display",    //ヘッダーのキャプション
+        height : 200,                  //高さ
+        width : 500,                   //幅
+        pager : 'mainPager',              //footerのページャー要素のid
+        shrinkToFit : true,
+        viewrecords: true              //footerの右下に表示する。
+});
+
+
+    $('#btnFileSelect').val('');
+    AssetManager.file = undefined;
+    AssetManager.reader = new FileReader();
+
+//    var dataset = [5,10,15,20,25];
+
+//    d3.select('body')
+//        .append('p')
+//        .text('新しいパラグラフ');
+
+//    d3.select()
+//        .data(dataset)
+//        .enter()
+//        .append('div')
+//        .attr('class','bar')
+//        .style('height', function(d) {
+//            return d * 5 + 'px';
+//        });
+	
+    $('#btnSave').bind('click', function() {
+        var name = getFileName();
+        var text = getCsvText();
+        JobExecuter.getDownloadFile(name, text, function(data) {
+            dwr.engine.openInDownload(data);
+        });
+
+        function getFileName() {
+            return 'asset_manager_' + getDateString() + '.csv';
+
+            function getDateString(date) {
+                if (!date) {
+                    date = new Date();
+                }
+                var yyyy =  date.getFullYear();
+                var mm = toDoubleDigits(date.getMonth() + 1);
+                var dd = toDoubleDigits(date.getDate());
+                return yyyy + mm + dd;
+            }
+
+            function toDoubleDigits(num) {
+                num += '';
+                if (num.length === 1) {
+                    num = '0' + num;
+                }
+                return num;
+            }
+        }
+
+        function getCsvText() {
+            var saveText = '';
+            $('body').find('.save').each(function(index) {
+                var elem = $(this);
+                if (saveText) {
+                    saveText += ',';
+                }
+                saveText += elem.attr('id') + ':' + elem.text();
+            });
+            return saveText;
+        }
+    });
+
+    $('#btnFileSelect').bind('change', function(event) {
+        var file = event.target.files[0];
+        if (canLoad(file))  {
+            AssetManager.file = file;
+        } else {
+            //TODO: ファイル選択状態を初期化することはできるのか。たぶんできるだろ。
+            throw new Error('Illegal File Error');
+        }
+
+        function canLoad(file) {
+            return true;
+        }
+        });
+
+    $('#btnFileLoad').bind('click', function() {
+        AssetManager.reader.readAsText(AssetManager.file, "UTF-8");
+    });
+
+    AssetManager.reader.onload = function(event) {
+        var values = [];
+        var result = event.target.result;
+        var dataArr = [];
+        values = result.split(',');
+        for(var i = 0; i < values.length; i++) {
+            var value = values[i];
+            var id = value.split(':')[0];
+            var value = value.split(':')[1];
+
+            var dataObj = {
+                dateVal: undefined,
+                planVal: undefined,
+                realVal: undefined
+            };
+
+            $('#' + id).text(value);
+        }
+        loadChart();
+    };
+
+    $('#btnLoadChart').bind('click', function() {
+        loadChart();
+
+
+//        function loadChart() {
+//            var canvas = d3.select('#chart');
+//            var svg = canvas.append('svg')
+//                .attr('width', 200)
+//                .attr('height', 200);
+//
+//            var dataArr = [
+//                {month: 1, count: 10},
+//                {month: 2, count: 20},
+//                {month: 3, count: 30},
+//                {month: 4, count: 40},
+//                {month: 5, count: 50},
+//                {month: 6, count: 60},
+//                {month: 7, count: 70},
+//                {month: 8, count: 80}
+//            ];
+//
+//            var scaleX = d3.scale.linear()
+//                .domain([0, dataArr.length])
+//                .range([0, 200]);
+//            var scaleY = d3.scale.linear()
+//                .domain([0, d3.max(dataArr, function(d) {return d.count;})])
+//                .range([10, 200]);
+//
+//            var area = d3.svg.area()
+//                .x(function(d, i) {
+//                    return scaleX(i);
+//                }).y0(function(d, i) {
+//                    return scaleY(d3.max(dataArr, function(d) {return d.count;}) - d.count + 10);
+//                }).y1(function(d, i) {
+//                    return scaleY(d3.max(dataArr, function(d) {return d.count;}));
+//                });
+//
+//            svg.append('path')
+//                .attr('d', area(dataArr))
+//                .attr('fill', 'aqua');
+//
+//            var line = d3.svg.line()
+//                .x(function(d,i) {
+//                    return scaleX(i);
+//                }).y(function(d,i) {
+//                    return scaleY(d3.max(dataArr, function(d){return d.count;}) - d.count + 10);
+//                }).interpolate('linear');
+//            svg.append('path')
+//                .attr('d', line(dataArr))
+//                .attr('stroke', 'blue')
+//                .attr('stroke-width', 2)
+//                .attr('fill', 'none');
+//        }
+    });
+
+    function loadChart() {
+        var dataArr = [];
+        $('.tableAll tr').each(function(index) {
+            var elem = $(this);
+            var data = {
+                date: undefined,
+                planVal: undefined,
+                realVal: undefined
+            };
+            if (elem.data('date')) {
+                data.date = elem.data('date');
+                data.planVal = parseInt(elem.children('.plan').text());
+                data.realVal = parseInt(elem.children('.real').text());
+                dataArr.push($.extend({}, data));
+            }
+        });
+        dataArr.sort(
+            function(a,b){
+                if(a.date < b.date ) return -1;
+                if( a.date > b.date ) return 1;
+                return 0;
+            }
+        );
+
+        var maxVal_X = d3.max(dataArr, function(d) {return d.planVal > d.realVal ? d.planVal : d.realVal;});
+
+        var scaleX = d3.scale.linear()
+            .domain([0, dataArr.length])
+            .range([0, 500]);
+        var scaleY = d3.scale.linear()
+            .domain([0, maxVal_X])
+            .range([0, 200]);
+
+        var canvas = d3.select('#chart');
+        var svg = canvas.append('svg')
+            .attr('width', 500)
+            .attr('height', 200);
+
+
+        drowLine('plan', 'blue');
+        drowLine('real','green');
+//
+//        svg.append('path')
+//            .attr('d', lineReal)
+////            .attr('d', linePlan)
+//            .attr('stroke', 'blue')
+//            .attr('stroke-width', 2)
+//            .attr('fill', 'none');
+
+        function drowLine(type, color) {
+//
+//        var area = d3.svg.area()
+//            .x(function(d, i) {
+//                return scaleX(i);
+//            }).y0(function(d, i) {
+//                return scaleY(d3.max(dataArr, function(d) {return d.realVal;}) - d.realVal);
+//            }).y1(function(d, i) {
+//                return scaleY(d3.max(dataArr, function(d) {return d.realVal;}));
+//            });
+//
+//        svg.append('path')
+//            .attr('d', area(dataArr))
+//            .attr('fill', 'aqua');
+
+            var line = d3.svg.line()
+                .x(function(d,i) {
+                    return scaleX(i);
+                }).y(function(d,i) {
+                    return scaleY(maxVal_X - getVal(d));
+                }).interpolate('linear');
+
+            svg.append('path')
+                .attr('d', line(dataArr))
+//            .attr('d', linePlan)
+                .attr('stroke', color)
+                .attr('stroke-width', 2)
+                .attr('fill', 'none');
+
+            return $.extend({}, line(dataArr));
+//            return $.extend({}, line);
+
+            function getVal(data) {
+                if (type == 'plan') {
+                    return data.planVal;
+                } else if (type == 'real') {
+                    return data.realVal;
+                }
+            }
+
+        }
+
+    }
+
+});
